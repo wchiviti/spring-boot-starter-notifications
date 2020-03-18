a2493eba7b1948fa2fa51837c9c37ce71fae150f
Wilson Chiviti <wilsonchiviti@gmail.com>
2020-03-18 16:52:25 +0200

    removing free marker

--- src/main/java/io/github/hobbstech/commons/notifications/core/EmailHtmlRepresentationBuilder.java
+++ src/main/java/io/github/hobbstech/commons/notifications/core/EmailHtmlRepresentationBuilder.java
@@ -1,17 +1,9 @@
 package io.github.hobbstech.commons.notifications.core;
 
 import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.ModelMap;
-import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
-
-import java.io.IOException;
-import java.time.LocalDate;
-import java.util.Map;
 
 @Slf4j
 @Service
@@ -28,25 +20,25 @@ public class EmailHtmlRepresentationBuilder {
 
     public String buildHtmlRepresentation(String message) {
 
-        Map<String, Object> model = new ModelMap();
+//        Map<String, Object> model = new ModelMap();
+//
+//        model.put("message", message);
+//        model.put("systemName", systemName);
+//        model.put("year", String.valueOf(LocalDate.now().getYear()).replace(",", ""));
+//
+//        String htmlString = "";
+//
+//        try {
+//
+//            Template t = freeMarkerConfiguration.getTemplate("classpath:email-template.ftl");
+//
+//            htmlString = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+//
+//        } catch (IOException | TemplateException e) {
+//            log.error("Failed to create an html representation of the email message due to : {}", e.getMessage());
+//        }
 
-        model.put("message", message);
-        model.put("systemName", systemName);
-        model.put("year", String.valueOf(LocalDate.now().getYear()).replace(",", ""));
-
-        String htmlString = "";
-
-        try {
-
-            Template t = freeMarkerConfiguration.getTemplate("email-template.ftl");
-
-            htmlString = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
-
-        } catch (IOException | TemplateException e) {
-            log.error("Failed to create an html representation of the email message due to : {}", e.getMessage());
-        }
-
-        return htmlString;
+        return message;
 
     }
 
