fe7ab19052f77313d17bb6bcdb1000649af67cc4
Wilson Chiviti <wilsonchiviti@gmail.com>
2020-04-05 12:20:23 +0200

    enabling feign clients for notifications

--- src/main/java/io/github/hobbstech/commons/notifications/NotificationsConfiguration.java
+++ src/main/java/io/github/hobbstech/commons/notifications/NotificationsConfiguration.java
@@ -1,9 +1,11 @@
 package io.github.hobbstech.commons.notifications;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "io.github.hobbstech.commons.notifications")
+@EnableFeignClients(basePackages = "io.github.hobbstech.commons.notifications")
 public class NotificationsConfiguration {
 }
